2f6ede199a7c9c49545253c7bb09f38dccb39857
Jenzi <jennydinh94@gmail.com>
2023-02-09 17:32:16 +0100

    Create models

---
+++ CD_Collection/Models/CD.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace CD_Collection.Models
+{
+	public class Album
+	{
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public int Tracks { get; set; }
+    }
+
+    public class Artist
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? BirthDate { get; set; }
+    }
+}
+
