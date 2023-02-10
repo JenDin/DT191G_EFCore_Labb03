95dc6af60af3c359f6de491ab4c9627f5b01cd43
Jenzi <jennydinh94@gmail.com>
2023-02-10 13:21:36 +0100

    Add data annotations for the model

--- CD_Collection/Models/CD.cs
+++ CD_Collection/Models/CD.cs
@@ -1,16 +1,21 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace CD_Collection.Models
 {
 	public class Album
 	{
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "* Enter a title")]
         public string? Title { get; set; }
 
+        [Required(ErrorMessage = "* Enter amount of tracks")]
         public int Tracks { get; set; }
 
         public Artist? Artist { get; set; }
 
+        [Required(ErrorMessage = "* Enter an artist name")]
         public int ArtistId { get; set; }
     }
 
@@ -18,8 +23,11 @@ namespace CD_Collection.Models
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "* Enter a name")]
         public string? Name { get; set; }
 
+        [Required(ErrorMessage = "* Enter a date of birth")]
+        [Display(Name = "Date of birth YYYY-MM-DD")]
         public string? BirthDate { get; set; }
 
         public List<Album>? Album { get; set; }
